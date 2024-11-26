5045d5b061042be561de2ec8e03305e0d4656b34
fuadaeff <fabdulla057@gmail.com>
2024-11-26 17:08:42 +0400

    Add files via upload

---
+++ HomeWork1/HomeWork1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeWork1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int C;
+            Console.WriteLine("Input your Farenheigt degree;");
+            int F = Convert.ToInt32(Console.ReadLine());
+            C = ((F - 32) * 5) / 9;
+            Console.WriteLine("Your Celsius degree is " + C);
+        }
+    }
+}
