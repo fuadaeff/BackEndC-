e32f62ae0ae7b8e00d42c270555af40ec61c022a
fuadaeff <fabdulla057@gmail.com>
2024-11-26 17:11:24 +0400

    Add files via upload

---
+++ HomeWork3/HomeWork3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ilk ededi daxil edin:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ucuncu ededi daxil edin:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int S;
+            S = (a + b + c) / 3;
+            Console.WriteLine(S);
+        }
+    }
+}
