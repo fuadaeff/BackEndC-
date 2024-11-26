d4e4fdc4cef0c7d5df5cf53bb46056408ceb1088
fuadaeff <fabdulla057@gmail.com>
2024-11-26 17:09:39 +0400

    Add files via upload

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ilk ededi daxil edin:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            /* or we can do it with adding third condition;
+             * int c;
+             * c = a;a=b;b=c
+             * and print
+            */
+            Console.WriteLine(b);
+            Console.WriteLine(a);
+            
+        }
+    }
+}
